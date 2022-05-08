3c57fe3b1ff9e3da5f7834e915e5f871bb0067cc
Recepguc <recep.gucc@gmail.com>
2022-05-08 16:33:58 +0300

    homejs upt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,42 +1,62 @@
-import axios from 'axios';
-import React from 'react'
-import Header from '../../compenents/header/Header'
-import {useState, useEffect} from 'react'
+import axios from "axios";
+import React from "react";
+import Header from "../../compenents/header/Header";
+import { useState, useEffect } from "react";
+import { HomeImg, ImgDiv, MainContainer } from "./HomeStyles";
+import RecipeCardComp from "./RecipeCardComp";
+import homeSvg from "../../assets/home.svg";
 
-
-const APP_ID = "0fcc65b4"
-const APP_KEY = "38c8b829fcc3757fd9ef6774d839916e	—" ;
+const APP_ID = "0fcc65b4";
+const APP_KEY = "38c8b829fcc3757fd9ef6774d839916e	—";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [food, setFood] = useState()
-  const mealTypes=["Breakfast","Lunch","Dinner","Snack", "Teatime"] 
+  const [food, setFood] = useState();
+  const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
 
-  const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`
-  const getData=async()=>{
-    if(query){
-      const result = await axios.get(url)
-      setFood(result.data.hits)
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+
+  const getData = async () => {
+    if (query) {
+      const result = await axios.get(url);
+      setFood(result.data.hits);
       console.log(result.data.hits);
-      
     }
-    
-  }
+  };
+
   useEffect(() => {
-    getData()
-  }, [])
-  
-  
+    getData();
+  }, []);
 
   return (
     <div>
-      <Header setQuery={setQuery}
-      getData={getData }
-      mealTypes={mealTypes}
-      setMeal={setMeal}/> 
-    </div>
-  )
-}
+      <Header
+        setQuery={setQuery}
+        getData={getData}
+        mealTypes={mealTypes}
+        setMeal={setMeal}
+      />
 
-export default Home
+      {food ? (
+        // api den gelen datanın tümü, (bir yiyecek ismi girildiğinde)
+        <MainContainer>
+          {/* arama sonucu yiyecek kartlarının kutusu */}
+          {food.map((liste, index) => (
+            <RecipeCardComp key={index} recipe1={liste.recipe} />
+            // sadece liste yollarsak apı deki gibi  gidiyor. liste.recipe= json formatında key value. kısaca{recipe}.hits in içinde recipe var, liste hits e kadar gelir ben içindeki recipe'yi istiyorum
+            // veriler direk bu sayfaya geliyor ,RecipeCardComp deki gibi navigate state e gerek yok
+          ))}
+        </MainContainer>
+      ) : (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+
+          {/* ekrandaki aşçı resmi */}
+        </ImgDiv>
+      )}
+    </div>
+  );
+};
+
+export default Home;
