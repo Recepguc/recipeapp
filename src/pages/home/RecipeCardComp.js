8ff786d8e5a4b0fa261721b3223cd55f36f14376
Recepguc <recep.gucc@gmail.com>
2022-05-08 17:30:21 +0300

    recipe js upt

--- src/pages/home/RecipeCardComp.js
+++ src/pages/home/RecipeCardComp.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button, RecipeCard, RecipeHeader, RecipeImage } from "./HomeStyles";
 
-const RecipeCardComp = () => {
-  return <div>RecipeCardComp</div>;
+const RecipeCardComp = ({ recipe1 }) => {
+  return (
+    <RecipeCard>
+      <RecipeHeader>{recipe1.label}</RecipeHeader>
+      <RecipeImage src={recipe1.image} />
+      <Button>Viev More</Button>
+    </RecipeCard>
+  );
 };
 
 export default RecipeCardComp;
