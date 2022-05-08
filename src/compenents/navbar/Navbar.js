3222e0f685872301d6b8d2aa3d30eddcc4773b16
Recepguc <recep.gucc@gmail.com>
2022-05-08 17:29:49 +0300

    navbar js upt

--- src/compenents/navbar/Navbar.js
+++ src/compenents/navbar/Navbar.js
@@ -1,29 +1,31 @@
-import React, {useState} from 'react'
-import { Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyled'
+import React, { useState } from "react";
+import { Hamburger, Logo, Menu, MenuLink, Nav } from "./NavbarStyled";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <Nav>
-      <Logo onClick={()=>setOpen(!open)} to="/home">
+      <Logo onClick={() => setOpen(!open)} to="/home">
         <i>{"<RecepGüç>"}</i>
         <span>recipe</span>
-        <Hamburger onClick={()=>setOpen(!open)}>
-          <span/>
-          <span/>
-          <span/>
-        </Hamburger>
       </Logo>
+      <Hamburger onClick={() => setOpen(!open)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+
       <Menu open={open}>
-        <MenuLink onClick={()=>setOpen(!open)} to="/abaout">About</MenuLink>
+        <MenuLink onClick={() => setOpen(!open)} to="/abaout">
+          About
+        </MenuLink>
         <MenuLink to="/github">Github</MenuLink>
-        <MenuLink onClick={()=>setOpen(!open)} to="/Logout">Logout</MenuLink>
+        <MenuLink onClick={() => setOpen(!open)} to="/Logout">
+          Logout
+        </MenuLink>
       </Menu>
     </Nav>
-    
-       
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
