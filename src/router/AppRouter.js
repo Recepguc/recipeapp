22cb7b3d5323d96cffff498fc81648277d25bb8d
Recepguc <recep.gucc@gmail.com>
2022-05-11 22:45:22 +0300

    Approuter upt

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Navbar from "../compenents/navbar/Navbar"
-import Footer from '../compenents/footer/Footer'
-import Login from "../pages/login/Login"
-import Home from  "../pages/home/Home"
-import About from  "../pages/about/About"
-import Details from  "../pages/details/Details"
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../compenents/navbar/Navbar";
+import Footer from "../compenents/footer/Footer";
+import Login from "../pages/login/Login";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Details from "../pages/details/Details";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/details" element={<Details/>}/>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<PrivateRouter />}>
+            <Route path="" element={<Home />} />
+          </Route>
 
-
+          <Route path="/about" element={<PrivateRouter />}>
+            <Route path="" element={<About />} />
+          </Route>
+          <Route path="/details" element={<Details />} />
         </Routes>
-        
 
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-        
     </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
